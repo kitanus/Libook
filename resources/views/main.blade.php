4113320d05512964f06c2a3413e97fa35458cbe3
kitanus <kulukafarmin@mail.ru>
2019-09-15 13:36:04 +0300

    add color and change making grid

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,35 +16,41 @@
             <div id="logo">Logo</div>
             <div id="name">Libook</div>
             <div id="user">
-                <div id="enter">Enter</div>
-                <div id="login">Login</div>
+                <div id="enter">Вход</div>
+                <div id="login">Регистрация</div>
             </div>
         </header>
         <div id="menu">
-            <div id="a-main">a-main</div>
-            <div id="a-list">a-list</div>
-            <div id="a-search">a-search</div>
+            <div id="a-main">Главная</div>
+            <div id="a-list">Список</div>
+            <div id="a-search">Расширенный поиск</div>
             <form action="">
-                    <input type="text" value="text">
-                    <button name="action">button</button>
+                    <input type="text" placeholder="Поиск">
+                    <button name="action">Найти</button>
             </form>
             </div>
         </div>
-        <main>
-            <div id="news-block">
-                <div id="name">newsName</div>
-                <div id="blockNews">
-                    <div class="content">blockNews</div>
-                    <div class="content">blockNews</div>
-                </div>
+        <div id="news-block">
+            <div id="name">Новостной блок</div>
+            <div id="blockNews">
+                <div id="first-news-name">Первая новость</div>
+                <div id="first-news">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</div>
+                <a id="first-news-end">Читать дальше...</a>
+                <div id="second-news-name">Вторая новость</div>
+                <div id="second-news">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</div>
+                <a id="second-news-end">Читать дальше...</a>
             </div>
-            <div id="sidebar">
-                <div id="name">sb name</div>
-                <div id="blockList">
-                    <div id="list">List</div>
-                </div>
+        </div>
+        <div id="sidebar">
+            <div id="name">Последние добавленные книги</div>
+            <div id="blockList">
+                <ul id="list">
+                    <li><a href="/">Пункт 1</a></li>
+                    <li><a href="/">Пункт 1</a></li>
+                    <li><a href="/">Пункт 1</a></li>
+                </ul>
             </div>
-        </main>
-        <footer></footer>
+        </div>
+        <footer>FOOTER</footer>
     </body>
 </html>

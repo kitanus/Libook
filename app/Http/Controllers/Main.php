4ca6fc25167413bcba7c35eabf70f4065f0ca269
kitanus <kulukafarmin@mail.ru>
2020-01-15 17:36:07 +0300

    add news to bd in main page

--- app/Http/Controllers/Main.php
+++ app/Http/Controllers/Main.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class Main extends Controller
 {
     public function index()
     {
-        return view('main');
+        $news = DB::table('news')->get();
+
+        $lastNews = $news[count($news)-1];
+        $preLastNews = $news[count($news)-2];
+
+        $arrNews = [
+            $this->getTextNews('news/'.$preLastNews->addres_text.'.txt'),
+            $this->getTextNews('news/'.$lastNews->addres_text.'.txt')
+        ];
+
+        $arrNamesNews = [
+            $preLastNews->name,
+            $lastNews->name
+        ];
+
+        return view('main', [
+            'news' => $arrNews,
+            'nameNews' => $arrNamesNews
+        ]);
+    }
+
+    public function getTextNews($path)
+    {
+        return explode("\n", Storage::disk('public')->get($path));
     }
 }
--- app/Http/Controllers/News.php
+++ app/Http/Controllers/News.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class News extends Controller
 {
     public function index()
     {
-        return view('news');
+        if(!$_GET)
+        {
+            $news = DB::table('news')->where('id', '1')->first();
+            $id = 1;
+        }
+        else
+        {
+            $news = DB::table('news')->where('id', $_GET['news_id'])->first();
+            $id = $_GET['news_id'];
+
+            if(empty($news))
+            {
+                $news = DB::table('news')->where('id', '1')->first();
+                $id = 1;
+            }
+        }
+
+        return view('news', [
+            'content' => $this->getTextNews('news/'.$news->addres_text.'.txt'),
+            'name' => $news->name,
+            'id' => $id
+        ]);
+    }
+
+    public function getTextNews($path)
+    {
+        return explode("\n", Storage::disk('public')->get($path));
     }
 }
